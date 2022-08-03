4ba4830e4e5329cc2a36f19bcf3fdfc8d132203b
KJ Johnson <kjohns95@emich.edu>
2022-08-03 18:29:35 -0400

    syling the trashcan isn't working

--- src/components/PostInList.tsx
+++ src/components/PostInList.tsx
@@ -1,10 +1,19 @@
 import { Post } from "../App";
 import "./styling.css";
+// import DeleteSweepIcon from "@mui/icons-material/DeleteSweep";
 
 interface PostProp {
   post: Post;
   onDelete: () => void;
 }
+
+// function HomeIcon(props) {
+//   return (
+//     <SvgIcon {...props}>
+//       <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
+//     </SvgIcon>
+//   );
+// }
 
 export default function PostInList({ post, onDelete }: PostProp) {
   return (
@@ -12,7 +21,7 @@ export default function PostInList({ post, onDelete }: PostProp) {
       <div className="post-css">
         <h1>{post.title}</h1>
         <p>{post.thought}</p>
-        <button onClick={() => onDelete()}>TrashCan</button>
+        <button onClick={() => onDelete()}>TRASHCAN</button>
       </div>
     </div>
   );
